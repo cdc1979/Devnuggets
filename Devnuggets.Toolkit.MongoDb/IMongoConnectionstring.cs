a212b269d7b90665c4088aff9ba2aa46f2b3fc47
Chris <chris.d.casey@googlemail.com>
2014-02-14 10:39:33 +0000

    Added some mongodb helper classes

---
+++ Devnuggets.Toolkit.MongoDb/IMongoConnectionstring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devnuggets.Toolkit.MongoDb
+{
+    public interface IMongoConnectionstring
+    {
+        string GetConnectionstring();
+    }
+
+    public class MongoConnectionStringFromWebConfig : IMongoConnectionstring
+    {
+        public string GetConnectionstring()
+        {
+            return ConfigurationManager.ConnectionStrings["MongoServerSettings"].ConnectionString;
+        }
+    }
+}
---
+++ Devnuggets.Toolkit.MongoDb/MongoConnection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+namespace Devnuggets.Toolkit.MongoDb
+{
+    public class MongoConnection : IDisposable
+    {
+        private MongoDatabase _db;
+        private MongoServer _server;
+
+        // use MongoConnectionStringFromWebConfig class (uses connectionstring key "MongoServerSettings") or inject your own
+
+        public MongoConnection(string databaseName, IMongoConnectionstring mongoConnectionString)
+        {
+            MongoClient client = new MongoClient( mongoConnectionString.GetConnectionstring() );
+            _server =  client.GetServer();
+            _db = _server.GetDatabase(databaseName);
+        }
+
+        public bool CreateIndex(string collection, string[] fieldnames)
+        {
+            _db.GetCollection(collection).EnsureIndex(fieldnames);
+            return true;
+        }
+
+        public bool CopyCollection(string sourceCollection, string destinationCollection) 
+        {
+            if (_db.CollectionExists(destinationCollection)) 
+            {
+                throw new Exception("Destination collection already exists");
+            }
+            var source = _db.GetCollection(sourceCollection);
+            var dest = _db.GetCollection(destinationCollection);
+            dest.InsertBatch(source.FindAll());
+            return true;
+        }
+
+        public CommandResult CompactCollection(string collectionname)
+        {
+            
+            return _db.RunCommand(new CommandDocument("compact", collectionname));
+        }
+
+        public DatabaseStatsResult GetStats()
+        {
+            return _db.GetStats();
+        }
+
+        public CollectionStatsResult GetStats(string collectionName)
+        {
+            return _db.GetCollection(collectionName).GetStats();
+        }
+
+        public CommandResult RepairDatabase()
+        {
+            return _db.RunCommand(new CommandDocument("repairDatabase", 1));
+        }
+
+        public void Dispose()
+        {
+            // no need to disconnect, driver handles it
+        }
+    }
+}
